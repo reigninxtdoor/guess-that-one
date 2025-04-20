3ea14d5f5269eaeed8d9ed31f77e2ff6c01e2607
nazir martin <155297402+reigninxtdoor@users.noreply.github.com>
2025-04-20 01:36:02 +0000

    couple of vars added

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
 const word = "magnolia";
-
+let guessedLetters=[];
 // Display our symbols as placeholders for the chosen word's letters
 const placeholder = function (word) {
   const placeholderLetters = [];
@@ -26,4 +26,29 @@ guessLetterButton.addEventListener("click", function (e) {
   const guess = letterInput.value;
   console.log(guess);
   letterInput.value = "";
+  message.innerText="";
 });
+const inputValve= function(input){
+  const acceptedLetter = /[a-zA-Z]/;
+    if(input.length ===0){
+      message.innerText="plz enter a letter ";
+      // if it is empty
+    }
+    else if(input.length>1){
+message.innerText="too many letters selected.";
+//more than 1 selected
+    }
+else if (input.length(acceptedLetter)){
+  message.innerText="wrong character selected, only letters plz";
+  // a none letter entered 
+}
+else{
+  message.innerText="youve entered a letter, congradulations"
+}
+return input;
+placeholder();
+  }
+const makeGuess=function(letter){
+  console.log(guessedLetters);
+  
+}
